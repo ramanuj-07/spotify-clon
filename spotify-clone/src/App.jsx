bc32708a3da6f8eb7343863b30ddf881a04cf8a5
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-07-14 22:32:00 +0530

    Update Sidebar.jsx

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Sidebar from './components/Sidebar';
+import Player from './components/Player';
 
 const App = () => {
   return (
-    <div className='' >
-      
-      App
-
+    <div className='h-screen bg-black' >
+      <div className='h-[90%] flex'>
+        <Sidebar/>
+      </div>
     </div>
   )
 }
